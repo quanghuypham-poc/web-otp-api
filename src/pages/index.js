26d707db8a2ab6c0647002f9a465d9ed8abecda2
quanghuypham-poc <quanghuy.pham@pharmapoc.com>
2023-07-14 14:04:06 +0700

    chore: update

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
 
   useEffect(() => {
     document.getElementById("input").addEventListener("focusout", () => {
-      setOtp(otp);
+      if (otp) setOtp(otp);
       setSubmit("focusout");
     });
 
