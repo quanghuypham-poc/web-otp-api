c1dac57018494510f74d85b727f4b1b16880957c
quanghuypham-poc <quanghuy.pham@pharmapoc.com>
2023-07-26 15:30:25 +0700

    chore: update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,22 @@
 import { useEffect, useRef, useState } from "react";
 
+const OTP_CODE_LENGTH = 6;
+
 export default function Home() {
   const [otp, setOtp] = useState("");
+  const [screen, setScreen] = useState("phone");
 
   const inputRef = useRef(null);
 
   const handleChange = (event) => {
     event.preventDefault();
-    console.log("change event.data", event.data);
-    setOtp(event.target.value);
-  };
-
-  const handleInput = (event) => {
-    console.log("event", event);
-    event.preventDefault();
-    event.target.value = event.data;
-    console.log("input", event.data);
-    setOtp(event.target.value);
+    const { value } = event.target;
+    const newValue = value.slice(-OTP_CODE_LENGTH);
+    setOtp(newValue);
   };
 
   useEffect(() => {
-    if ("OTPCredential" in window) {
+    if ("OTPCredential" in window && screen === "otp") {
       const ac = new AbortController();
 
       navigator.credentials
@@ -29,6 +25,9 @@ export default function Home() {
           signal: ac.signal,
         })
         .then((otp) => {
+          if (otp && screen !== "otp") {
+            return;
+          }
           setOtp(otp.code);
           ac.abort;
         });
@@ -39,17 +38,22 @@ export default function Home() {
     <div style={{ display: "flex", flexDirection: "column" }}>
       <h1>Web Otp Example Code</h1>
 
-      <input
-        id="otp-input"
-        ref={inputRef}
-        inputMode="numeric"
-        value={otp}
-        onInput={handleInput}
-        autoComplete="one-time-code"
-        maxLength={6}
-      />
-
+      <div>Screen: {screen}</div>
+      {screen === "phone" ? (
+        <div>blah blah</div>
+      ) : (
+        <input
+          id="otp-input"
+          ref={inputRef}
+          inputMode="numeric"
+          value={otp}
+          onChange={handleChange}
+          autoComplete="one-time-code"
+        />
+      )}
       <span>OTP: {otp}</span>
+      <button onClick={() => setScreen("phone")}>Back</button>
+      <button onClick={() => setScreen("otp")}>Next</button>
     </div>
   );
 }
