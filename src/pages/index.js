498b8b817d916d74c52aff18b6de93d88b6ff5ab
quanghuypham-poc <quanghuy.pham@pharmapoc.com>
2023-07-26 16:04:10 +0700

    chore: update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ const OTP_CODE_LENGTH = 6;
 export default function Home() {
   const [otp, setOtp] = useState("");
   const [text, setText] = useState("");
-  const [screen, setScreen] = useState("phone");
 
   const inputRef = useRef(null);
 
@@ -14,13 +13,14 @@ export default function Home() {
     const { value } = event.target;
     const newValue = value.slice(-OTP_CODE_LENGTH);
     setOtp(newValue);
+
     if (inputRef.current.value.length === OTP_CODE_LENGTH) {
       setText("submit");
     }
   };
 
   useEffect(() => {
-    if ("OTPCredential" in window && screen === "otp") {
+    if ("OTPCredential" in window) {
       const ac = new AbortController();
 
       navigator.credentials
@@ -29,11 +29,7 @@ export default function Home() {
           signal: ac.signal,
         })
         .then((otp) => {
-          if (otp && screen !== "otp") {
-            return;
-          }
           setOtp(otp.code);
-          console.log("ref", inputRef.current);
           ac.abort;
         });
     }
@@ -43,19 +39,15 @@ export default function Home() {
     <div style={{ display: "flex", flexDirection: "column" }}>
       <h1>Web Otp Example Code</h1>
 
-      <div>Screen: {screen}</div>
-      {screen === "phone" ? (
-        <div>blah blah</div>
-      ) : (
-        <input
-          id="otp-input"
-          ref={inputRef}
-          inputMode="numeric"
-          value={otp}
-          onChange={handleChange}
-          autoComplete="one-time-code"
-        />
-      )}
+      <input
+        id="otp-input"
+        ref={inputRef}
+        inputMode="numeric"
+        value={otp}
+        onChange={handleChange}
+        autoComplete="one-time-code"
+      />
+
       <span>OTP: {otp}</span>
       <span>Text: {text}</span>
     </div>
